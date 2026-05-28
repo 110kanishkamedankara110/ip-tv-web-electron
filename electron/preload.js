946c65aaebac6c41a6e1045aff72b099527d8d0e
kanishka medankara <110kqnishkamedankara110@gmail.com>
2026-05-28 15:50:00 +0530

    FEAT :: app improvments and playlist edits

--- electron/preload.js
+++ electron/preload.js
@@ -2,8 +2,7 @@ const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("electronAPI", {
   setWindowSize: (w, h) => ipcRenderer.invoke("set-window-size", w, h),
-
+  fetchM3U: (url) => ipcRenderer.invoke("fetch-m3u", url),
   playInVLC: (url) => ipcRenderer.invoke("play-vlc", url),
-
   stopVLC: () => ipcRenderer.invoke("stop-vlc"),
 });
